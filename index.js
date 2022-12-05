df28cd00d8e96009e7929fe206e190afc8f5e0f7
monosniper <vanteateam@gmail.com>
2022-12-05 23:44:23 +0500

    cors

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const corsOptions = {
 //     credentials: true,
 //     origin: 'https://makao777.com'
 // }));
-app.options('/api', cors(corsOptions));
+app.use(cors(corsOptions));
 app.use('/api', router);
 app.use(errorMiddleware);
 app.use(express.static('uploads', {
